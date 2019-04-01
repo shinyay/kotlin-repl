57dc8f0b10e0914f51441be6f269e9d085c85c9b
shinyay <shinya.com@gmail.com>
2019-04-01 20:11:05 +0900

    Add kotlin sample app run by gradle

--- src/main/kotlin/io/pivotal/shinyay/Hello.kt
+++ src/main/kotlin/io/pivotal/shinyay/Hello.kt
@@ -1,4 +1,13 @@
 package io.pivotal.shinyay
 
-class Hello {
+fun getGreeting(): String {
+    val words = mutableListOf<String>()
+    words.add("Hello,")
+    words.add("Kotlin!")
+
+    return words.joinToString(separator = " ")
+}
+
+fun main(args: Array<String>) {
+    println(getGreeting())
 }
