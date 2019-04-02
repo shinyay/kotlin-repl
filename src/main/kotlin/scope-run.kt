abb956c744cb72dbf15f783882229d60db028640
shinyay <shinya.com@gmail.com>
2019-04-02 14:04:51 +0900

    Add run definition

--- src/main/kotlin/scope-run.kt
+++ src/main/kotlin/scope-run.kt
@@ -1,4 +1,13 @@
 fun doRun() {
+
+    val function_definition = """
+        [run definition]
+        public inline fun <T, R> T.run(f: T.() -> R): R = f()
+
+    """.trimIndent()
+
+    println(function_definition)
+
     val s = "Lorem ipsum dolor sit amet".run { toUpperCase() }
     println(s)
 }
