d0564a93130c47444361e955572bfa5663fa1e59
shinyay <shinya.com@gmail.com>
2019-04-02 14:10:33 +0900

    Add also definition

--- src/main/kotlin/scope-also.kt
+++ src/main/kotlin/scope-also.kt
@@ -1,4 +1,13 @@
 fun doAlso() {
+
+    val function_definition = """
+        [also definition]
+        public inline fun <T> T.also(block: (T) -> Unit): T { block(this); return this }
+
+    """.trimIndent()
+
+    println(function_definition)
+
     val s = "Lorem ipsum dolor sit amet".also { it.toLowerCase() }
     println(s)
 }
