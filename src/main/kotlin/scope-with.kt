194056e079cdaf22659fd6a4ac3238fc8b32c0d9
shinyay <shinya.com@gmail.com>
2019-04-02 14:03:30 +0900

    Add with definition

--- src/main/kotlin/scope-with.kt
+++ src/main/kotlin/scope-with.kt
@@ -1,4 +1,13 @@
 fun doWith() {
+
+    val function_definition = """
+        [with definition]
+        public inline fun <T, R> with(receiver: T, f: T.() -> R): R = receiver.f()
+
+    """.trimIndent()
+
+    println(function_definition)
+
     val s = with("Lorem ipsum dolor sit amet") { this.reversed() }
     println(s)
 }
