b8c0672228a9e3e22bce641dc0c132b7e6d70466
shinyay <shinya.com@gmail.com>
2019-04-02 14:06:07 +0900

    Add apply definition

--- src/main/kotlin/scope-apply.kt
+++ src/main/kotlin/scope-apply.kt
@@ -1,4 +1,13 @@
 fun doApply() {
+
+    val function_definition = """
+        [apply definition]
+        public inline fun <T> T.apply(f: T.() -> Unit): T { f(); return this }
+
+    """.trimIndent()
+
+    println(function_definition)
+
     val s = "Lorem ipsum dolor sit amet".apply { toUpperCase() }
     println(s)
 }
