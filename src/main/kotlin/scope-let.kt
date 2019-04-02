f52763e1c9f7fc67405137b97c784094cb823d51
shinyay <shinya.com@gmail.com>
2019-04-02 14:02:09 +0900

    Add let definition

--- src/main/kotlin/scope-let.kt
+++ src/main/kotlin/scope-let.kt
@@ -1,4 +1,13 @@
 fun doLet() {
+
+    val function_definition = """
+        [let definition]
+        public inline fun <T, R> T.let(f: (T) -> R): R = f(this)
+
+    """.trimIndent()
+
+    println(function_definition)
+
     val s = "Lorem ipsum dolor sit amet".let { it.toLowerCase() }
     println(s)
 }
