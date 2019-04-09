f01e6676cfb0404a279ff9f74d67cf56bd9bc600
shinyay <shinya.com@gmail.com>
2019-04-09 20:57:45 +0900

    Modify default value as current directory

--- src/main/kotlin/utility.kt
+++ src/main/kotlin/utility.kt
@@ -1,5 +1,7 @@
 import java.io.File
 
-fun ls_R(dir: String = "src") {
+fun ls_R(dir: String = getCurrentDir()) {
     File(dir).walkTopDown().forEach { println(it) }
 }
+
+fun getCurrentDir() = File(".").absoluteFile.parent
