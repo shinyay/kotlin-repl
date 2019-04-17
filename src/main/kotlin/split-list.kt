3272c49a0f79b7a59812f3d672a71f8aa27c72ce
shinyay <shinya.com@gmail.com>
2019-04-18 07:47:20 +0900

    Modify kotlin syntax

--- src/main/kotlin/split-list.kt
+++ src/main/kotlin/split-list.kt
@@ -4,4 +4,6 @@ val oddList = listOf(0, "A", 1, "B", 2, "C", 3)
 
 fun <T> listGroupByWithIndex(list: List<T>) = list.withIndex()
             .groupBy { it.index / 2 }
-            .map { entry -> entry.value.map {it.value} }
+            .map { entry -> entry.value
+                    .map { it.value }
+            }
